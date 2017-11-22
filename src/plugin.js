d9d3594c7adf3bf2f74502a6d82a58f8d02a748c
memecast <=>
2017-11-21 21:55:36 -0600

    Primeros tres puntos resueltos, YA TIENE IMAGENgit add .git add .

--- src/plugin.js
+++ src/plugin.js
@@ -4,17 +4,14 @@
         let templateModal = `<div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
-          <h5 class="modal-title">${$(e.target).data("title")}</h5>
+          <h3 class="modal-title">${$(e.target).data("title")}</h3>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
           </div>
           <div class="modal-body">
-          <p>Modal body text goes here.</p>
-          </div>
-          <div class="modal-footer">
-          <button type="button" class="btn btn-primary">Save changes</button>
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+             <p>${$(e.target).data("description")}</p>
+             <img class="pin-img" src = "${e.target.src}"/>
           </div>
         </div>
         </div>`
@@ -25,7 +22,7 @@
       for( let pin of data){
         let id = pin.id+1;
         let contenedorPin = $(`<article class = "pin"/>`)  
-        let img = $(`<img src = "${pin.image_url}" class = "pin-img modal-trigger" data-usuario = ${pin.user} data-title = ${pin.title}>`).on("click",mostrarModal);
+        let img = $(`<img src = "${pin.image_url}" class = "pin-img modal-trigger" data-usuario = "${pin.user}" data-description = "${pin.description}" data-title = "${pin.title}" " >`).on("click",mostrarModal);
         let descripcion = $(`<p class = "pin-description">${pin.description}</p>`);
         let titulo = $(`<h3>${pin.title}</h3>`)
         let hashtag = $(`<p class = "pin-user">${pin.hashtag}</p>`);
