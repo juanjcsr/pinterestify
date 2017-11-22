39493fa8aa095a9f0c4e4dba2c9ad040aec1bbe4
AnaSalazar <anastephany.salazarguzman@gmail.com>
2017-11-21 21:28:19 -0600

    Agrega imagenes y datos respectivos, falta modal

--- src/plugin.js
+++ src/plugin.js
@@ -1,8 +1,30 @@
 (($) => {
-  
+
     $.fn.pinterestify = function (options) {
-  
       console.log( "plugin!!!", options);
+      const modal = $(`<div><img id="imageyoutube" src="" /></div>`)
+      $('body').append(modal);
+      modal.hide();
+
+      for (const pin of options.data) {
+
+        const description = $(`<p>${pin.description}</p>`);
+        const hashtag = $(`<h1>${pin.hashtag}</h1>`);
+        const autor = $(`<p>${pin.user}</p>`);
+        const img = $(`<img src="${pin.image_url}">`);
+
+        $('.imagenes').append(img);
+        $('.imagenes').append(description);
+        $('.imagenes').append(hashtag);
+        $('.imagenes').append(autor);
+
+        img.click(function(){
+          console.log("ya reconoce el click");
+
+        })
+      }
+
+
     }
-  
+
   })(jQuery);
