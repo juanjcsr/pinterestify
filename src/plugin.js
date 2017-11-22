50d4867a415a3f3d8f785cb3ad0a32d885fb895b
Pilar Figueroa <pilarfc@MacBook-Pro-de-Pilar.local>
2017-11-21 21:30:54 -0600

    estructura finalizada

--- src/plugin.js
+++ src/plugin.js
@@ -1,8 +1,31 @@
 (($) => {
-  
+
     $.fn.pinterestify = function (options) {
-  
-      console.log( "plugin!!!", options);
+
+        var item = $('#container');
+
+      for (let value of options.data) {
+        console.log(value);
+
+        var divContenedor = $('<div class="item"></div>');
+        var title = $(divContenedor).append(`<h2>${value.title}</h2>`);
+        var description = $(divContenedor).append(`<div>${value.description}</div>`);
+        var image = $(divContenedor).append(`<img src="${value.image_url}"/>`);
+        var user = $(divContenedor).append(`<div>${value.user}</div>`);
+        var username = $(divContenedor).append(`<div>${value.username}</div>`);
+        var hashtag = $(divContenedor).append(`<div>${value.hashtag}</div>`);
+
+        item.append(divContenedor);
+      }
+
+      $(function(){
+        $('#container').masonry({
+          // options
+          itemSelector : '.item'
+        });
+  });
+
+      // console.log( "plugin!!!", options);
     }
-  
+
   })(jQuery);
